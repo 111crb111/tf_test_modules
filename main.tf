24b3ebf62f2fff0014d9ea8344ed36b4b21f03db
Yaroslav Subota <111CRB111@gmail.com>
2022-11-13 12:51:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "rm" {
-	source  = "a15e86425ab6.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
-	version = "0.0.1"
+module "submodules" {
+	source  = "0f5e261347aa.test-env.scalr.com/env-svrcnchebt61e30/submodules/local"
+	version = "1.0.3"
 }
